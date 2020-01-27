1a86dada55a88f3df6495daffb2a6973e631a637
Lianne Van Westrhenen <lianne.vanwestrhenen@incentro.com>
2020-01-27 15:00:37 +0100

    add front-end

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welkom in de sport app!</h1>
+    </div>
+  );
+}
+
+export default App;
