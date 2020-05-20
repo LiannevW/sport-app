350a07d2cbd1f593b8596ac17612bbe3861d78d3
Lianne Van Westrhenen <lianne.vanwestrhenen@incentro.com>
2020-05-20 11:10:58 +0200

    add check to update or post a new test

--- client/src/context/test/testReducer.js
+++ client/src/context/test/testReducer.js
@@ -1,29 +1,29 @@
 import {
-    GET_TESTS,
-    ADD_TEST,
-    TEST_ERROR
-  } from '../types';
+  GET_TESTS,
+  ADD_TEST,
+  CHECK_TEST,
+  TEST_ERROR
+} from '../types';
 
-  export default (state, action) => {
-    switch (action.type) {
-      case GET_TESTS:
-        return {
-          ...state,
-          tests: action.payload,
-          loading: false
-        };
-      case ADD_TEST:
-        return {
-          ...state,
-          tests: [action.payloads],
-          loading: false
-        };
-      case TEST_ERROR:
-        return {
-          ...state,
-          error: action.payload
-        };
-      default:
-          return state;
-    }
-  };
+export default (state, action) => {
+  switch (action.type) {
+    case GET_TESTS:
+      return {
+        ...state,
+        tests: action.payload,
+        loading: false
+      };
+    case CHECK_TEST:
+      return {
+        ...state,
+        submitted: true
+      }
+    case TEST_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
